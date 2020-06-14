0ce63dece4a8476b309f7351f79f776fbf7389db
Victor Barros <victorbarros1130@gmail.com>
2020-06-14 00:15:28 -0300

    Add author route

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
+type author struct {
+	Name string `json:"name"`
+}
+
+type authors map[int]author
+
 var (
-	authors = make(map[int]string)
+	db      = authors{}
 	csvName = flag.String("csv", "Authors.csv", "Authors file")
 )
 
@@ -19,12 +30,15 @@ var (
 
 func main() {
 	flag.Parse() // `go run main.go -h` for help flag
-	loadAuthors()
-	fmt.Println(authors)
+
+	db.loadCsv(*csvName)
+
+	srv := newServer()
+	panic(srv.ListenAndServe())
 }
 
-func loadAuthors() {
-	csvFile, err := os.Open(*csvName)
+func (a authors) loadCsv(name string) {
+	csvFile, err := os.Open(name)
 	defer csvFile.Close()
 	if err != nil {
 		panic(err)
@@ -32,11 +46,55 @@ func loadAuthors() {
 
 	csvLines, err := csv.NewReader(csvFile).ReadAll()
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	for idx, line := range csvLines {
-		authors[idx] = line[0]
+		a[idx] = author{Name: line[0]}
 	}
-	delete(authors, 0) // First row is header
+	delete(db, 0) // First row is header
+}
+
+func newServer() *http.Server {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/",
+		func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("work-at-olist\n"))
+		}).Methods("GET")
+
+	r.HandleFunc("/authors", listAuthors).Methods("GET")
+	r.HandleFunc("/authors/{id:[0-9]+}", getAuthor).Methods("GET")
+
+	srv := &http.Server{
+		Addr:    ":8092",
+		Handler: r,
+	}
+	return srv
+}
+
+func listAuthors(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json") // TODO Isn't work: >>> requests.get("http://localhost:8092/authors").headers
+	if err := json.NewEncoder(w).Encode(db); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getAuthor(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	id, _ := strconv.Atoi(params["id"])
+
+	author, ok := db[id]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json") // TODO Isn't work: {'Date': 'Sun, 14 Jun 2020 00:07:29 GMT', 'Content-Length': '924', 'Content-Type': 'text/plain; charset=utf-8'}
+	if err := json.NewEncoder(w).Encode(author); err != nil {
+		fmt.Println(err)
+	}
 }
